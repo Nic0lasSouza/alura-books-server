674f823f03a07b71b897d6760ff0aeb78c87f4bc
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-04-26 01:04:09 -0300

    adicionando verificacoes no body

--- controladores/livro.js
+++ controladores/livro.js
@@ -31,9 +31,14 @@ function getLivro(req, res){
 function postLivro(req,res) {
     try{
         const livroNovo = req.body
-        insereLivro(livroNovo);
-        res.status(201)
-        res.send("Livro inserido com sucesso")
+        if(req.body.nome){
+            insereLivro(livroNovo);
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else{
+            res.status(422);
+            res.send("Campo nome é obrigatório");
+        }
     } catch(error){
         res.status(500);
         res.send(error.message);
@@ -45,8 +50,13 @@ function patchLivro(req, res) {
         const id = req.params.id;
         if(id && Number(id)){
             const body = req.body;
-            modificaLivro(body, id)
-            res.send("Item modificado com sucesso")
+            if(req.body.nome){
+                modificaLivro(body, id)
+                res.send("Item modificado com sucesso")
+            } else{
+                res.status(422);
+                res.send("Campo nome é obrigatório");
+            }
         } else{
             res.status(422); //dado não esperado
             res.send("Id invalido")
