38ff081388d31572bb5a6869dbc4f9b105e53fef
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-04-28 06:15:45 -0300

    adicioando a funcao de inserir livros e deixando clean code

--- services/favoritos.js
+++ services/favoritos.js
@@ -1,21 +1,26 @@
 const fs = require("fs");
-const livros  = JSON.parse(fs.readFileSync( "favoritos.json"));//pegar todos os valores
+const livrosFavoritos  = JSON.parse(fs.readFileSync( "favoritos.json"));//pegar todos os valores
+const Livros  = JSON.parse(fs.readFileSync( "livros.json"));
 
 function getTodosFavoritos(){
-    return livros;
+    return livrosFavoritos;
 }
 
 function deletaFavoritoId(id){
-    const livrosFiltrados = livros.filter(livro => livro.id !== id) //tirar o item que tem o id que foi recebido
+    const livrosFiltrados = livrosFavoritos.filter(livro => livro.id !== id); //tirar o item que tem o id que foi recebido
 
-    fs.writeFile('favoritos.json',JSON.stringify(livrosFiltrados)) //limpa o arquivo para escrever novamente
+    fs.writeFile('favoritos.json',JSON.stringify(livrosFiltrados)); //limpa o arquivo para escrever novamente
 }
 
 function insereFavorito(id){
-    
+    const livroInserido = Livros.find(livro => livro.id === id);
+    const novaListaDeLivrosFavoritos =[... livrosFavoritos, livroInserido];
+
+    fs.writeFile('favoritos.json',JSON.stringify(novaListaDeLivrosFavoritos));
 }
 
 module.exports={
     getTodosFavoritos, //exporta a função que retorna todos os favoritos
-    deletaFavoritoId
+    deletaFavoritoId, // deletar livro por id
+    insereFavorito     //adicionar um livro aos favoritos
 }
